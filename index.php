38b574983febcc700c3ed2acf529b3bfca7f5736
webrelab <webrelab@gmail.com>
2018-05-22 21:55:32 +0800

    Сделал проверку на phpsessid

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
  * Time: 21:01
  */
 
-if (isset($_COOKIE['username'])) {
+if (isset($_COOKIE['username']) || isset($_COOKIE['PHPSESSID'])) {
   session_start();
 } else {
   header('location: auth.php');
