1d0f3bfafdc856a52fa8c08180dd48722ffa1cc5
webrelab <webrelab@gmail.com>
2018-05-22 22:04:28 +0800

    Исправил username

--- index.php
+++ index.php
@@ -6,8 +6,22 @@
  * Time: 21:01
  */
 
-if (isset($_COOKIE['username']) || isset($_COOKIE['PHPSESSID'])) {
+if ((isset($_COOKIE['remember']) && $_COOKIE['remember']) || isset($_COOKIE['PHPSESSID'])) {
   session_start();
+  $username = $_SESSION['username'];
 } else {
   header('location: auth.php');
 }
+
+?>
+
+<!doctype html>
+<html>
+<head>
+
+</head>
+<body>
+  <h1>Главная страница</h1>
+  <div>Вы вошли как <?php echo $username; ?></div>
+</body>
+</html>
